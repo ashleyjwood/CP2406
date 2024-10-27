b753d869f522a1c8115871a6c556a6df653c1600
ashleyjwood <ashley.wood@my.jcu.edu.au>
2024-10-27 18:11:22 +1000

    Add prac 5 files

---
+++ Practicals/Prac 5/prac05_task04_ex8p1.cpp
@@ -0,0 +1,43 @@
+// #include <cstddef>
+// import <iostream>;
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+}
+
+// FROM page 251:
+// export module spreadsheet_cell;
+
+// export class SpreadsheetCell
+// {
+// public:
+// 	void setValue(double value);
+// 	double getValue() const;
+
+// private:
+// 	double m_value{ 0 };
+// };
+
+// export class SpreadsheetCell
+//{
+// public:
+//	void setValue(double value) { m_value = value; }
+//	double getValue() const { return m_value; }
+// private:
+//	double m_value{ 0 };
+// };
+
+// FROM: page 253
+// module spreadsheet_cell;
+// void SpreadsheetCell::setValue(double value)
+// {
+// 	m_value = value;
+// }
+
+// double SpreadsheetCell::getValue() const
+// {
+// 	return m_value;
+// }
