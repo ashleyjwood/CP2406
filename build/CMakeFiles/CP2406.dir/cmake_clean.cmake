648e6647109b9b95139e5f58617920d7f037cf79
ashleyjwood <ashley.wood@my.jcu.edu.au>
2024-10-27 17:57:48 +1000

    Update git settings

---
+++ build/CMakeFiles/CP2406.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CP2406.dir/main.cpp.obj"
+  "CMakeFiles/CP2406.dir/main.cpp.obj.d"
+  "CP2406.exe"
+  "CP2406.exe.manifest"
+  "CP2406.pdb"
+  "libCP2406.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CP2406.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CP2406.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CP2406.
+# This may be replaced when dependencies are built.
